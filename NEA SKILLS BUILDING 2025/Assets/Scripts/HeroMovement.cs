c23b01cb3abe51a08f6508df93759e2814231779
lemiecm01 <lemiec.m01@student.lmas.ac.uk>
2025-03-31 14:24:18 +0100

    page 40 creating prefabs

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5.1f;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow))
+		{
+			transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, 0);
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			rb.AddForce (Vector2.up * 450);
+		}
+	}
+
+	void resetPosition()
+	{
+		Debug.Log ("SPIKED RECIEVED!");
+		transform.SetPositionAndRotation (new Vector3 (-5.58f,1.34f, 0), Quaternion.identity);
+	}
+}
---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+	GameObject Hero;
+
+	// Use this for initialization
+	void Start () {
+		Hero = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter2D(Collision2D other) 
+	{
+		
+		Debug.Log ("SPIKED!");
+		Hero.SendMessage ("resetPosition");
+
+	}
+}
