01193b9c21bd7c8ae87e8e5e33c6d690927df689
lemiecm01 <lemiec.m01@student.lmas.ac.uk>
2025-05-02 15:38:25 +0100

    slide 120

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Dinoscene(){
+		Debug.Log ("Button A Clicked!");
+		SceneManager.LoadScene ("Animations");
+	}
+	public void tutorialevel(){
+		Debug.Log ("Button B Clicked!");
+		SceneManager.LoadScene ("Levels Tutorial");
+	}
+	public void Enemyscene(){
+		Debug.Log ("Button C Clicked!");
+		SceneManager.LoadScene ("enemy");
+	}
+	public void Navigation(){
+		Debug.Log ("Button D Clicked!");
+		SceneManager.LoadScene ("Navigation");
+	}
+
+	public void menubutton(){
+		Debug.Log ("button menu clicked!");
+		SceneManager.LoadScene ("Menu");
+	}
+}
+
