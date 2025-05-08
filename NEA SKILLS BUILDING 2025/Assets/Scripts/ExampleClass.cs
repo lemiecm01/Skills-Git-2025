dca1526acf3cfa800ade9eaf3ebdeff4fe4d4cfb
lemiecm01 <lemiec.m01@student.lmas.ac.uk>
2025-05-08 11:25:07 +0100

    trying two different cameras

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/ExampleClass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+	public Transform hero;
+	public Vector3 offset;
+
+	void Update () 
+	{
+		transform.position = new Vector3 (hero.position.x + offset.x, hero.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+	}
+}
---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/swapcamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swapcamera : MonoBehaviour {
+
+	public Camera minimap;
+	public Camera Maincamera;
+
+	void update(){
+		if (Input.GetKey (KeyCode.M)) {
+			Maincamera.enabled = false;
+			minimap.enabled = true;
+		} else if (Input.GetKey (KeyCode.N)) {
+			Maincamera.enabled = true;
+			minimap.enabled = false;
+		}
+	}
+}
