ba1fbf96162d95d4519df74168f0d152ddfd231d
lemiecm01 <lemiec.m01@student.lmas.ac.uk>
2025-06-23 14:23:39 +0100

    23/06/25 task

---
+++ NEA SKILLS BUILDING 2025/Assets/Scripts/DoorChanged.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DoorChanged : MonoBehaviour {
+
+	public Sprite doorBlueStroked;
+	public Sprite doorGreenStroke;
+	public Sprite doorRedStroked;
+	SpriteRenderer door;
+
+	void Start (){
+		door=GetComponent<SpriteRenderer> ();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Alpha1))
+		{
+			door.sprite = doorBlueStroked;
+		}
+		else if(Input.GetKey (KeyCode.Alpha2))
+		{
+			door.sprite=doorGreenStroke;
+		}
+		else if(Input.GetKey (KeyCode.Alpha3))
+		{
+			door.sprite=doorRedStroked;
+		}
+	}
+}
